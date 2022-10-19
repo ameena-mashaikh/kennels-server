a921613748801d186250d0e56b933b2b4a6ae2b8
Ameena Mashaikh <ameenamashaikh@gmail.com>
2022-10-19 17:18:14 -0400

    final finished product of chapter 3

--- models/customer.py
+++ models/customer.py
@@ -1,12 +1,9 @@
-import email
-
-
 class Customer():
 
     # Class initializer. It has 2 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, name, email,password):
+    def __init__(self, id, name, email = "", password = ""):
         self.id = id
         self.name = name
         self.email = email
--- views/customer_requests.py
+++ views/customer_requests.py
@@ -10,7 +10,6 @@ CUSTOMERS = [
 ]
 
 
-
 def get_all_customers():
     # Open a connection to the database
     with sqlite3.connect("./kennel.sqlite3") as conn:
@@ -42,11 +41,11 @@ def get_all_customers():
             # Note that the database fields are specified in
             # exact order of the parameters defined in the
             # Location class above.
-            customer = Customer(row['id'], row['name'], row['email'], row['password'])
+            customer = Customer(row['id'], row['name'],
+                                row['email'], row['password'])
 
             customers.append(customer.__dict__)
     return customers
-
 
 
 # Function with a single parameter
@@ -65,30 +64,32 @@ def get_single_customer(id):
             c.password
         FROM customer c 
         WHERE c.id = ?
-        """, ( id, ))
+        """, (id, ))
 
         # Load the single result into memory
         data = db_cursor.fetchone()
 
         # Create a customer instance from the current row
-        customer = Customer(data['id'], data['name'], data['email'], data['password'])
+        customer = Customer(data['id'], data['name'],
+                            data['email'], data['password'])
 
         return customer.__dict__
 
+
 def create_customer(customer):
-    #& Get the id value of the last customer in the list
+    # & Get the id value of the last customer in the list
     max_id = CUSTOMERS[-1]["id"]
 
-    #& Add 1 to whatever that number is
+    # & Add 1 to whatever that number is
     new_id = max_id + 1
 
-    #& Add an `id` property to the customer dictionary
+    # & Add an `id` property to the customer dictionary
     customer["id"] = new_id
 
-    #& Add the customer dictionary to the list
+    # & Add the customer dictionary to the list
     CUSTOMERS.append(customer)
 
-    #& Return the dictionary with `id` property added
+    # & Return the dictionary with `id` property added
     return customer
 
 
@@ -116,3 +117,30 @@ def update_customer(id, new_customer):
             # Found the CUSTOMERS. Update the value.
             CUSTOMERS[index] = new_customer
             break
+
+
+def get_customers_by_email(email):
+
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.email,
+            c.password
+        FROM Customer c
+        WHERE c.email = ?
+        """, (email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['email'], row['password'])
+
+            customers.append(customer.__dict__)
+    return customers
